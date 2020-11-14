c70bb90c0595ba8bd4059677cb5cb72a28db986b
melanas <anas.melk96@gmail.com>
2020-11-14 23:53:10 +0100

    added post route

---
+++ server/server.js
@@ -0,0 +1,24 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const {mongoose} = require('./db/mongoose');
+const {Todo} = require('./models/todo');
+const {User} = require('./models/user');
+
+let port = process.env.PORT || 3000;
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/todos',(req,res)=>{
+    let task = new Todo({text:req.body.text});
+
+    task.save()
+        .then(doc => res.send(doc))
+        .catch(err => res.status(400).send(err));
+});
+
+app.listen(port,()=> console.log('app listening on port: ',port));
+
+
