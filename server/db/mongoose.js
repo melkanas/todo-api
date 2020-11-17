34d1db8406004c92de6ee3de86c899795ff9ff46
melanas <anas.melk96@gmail.com>
2020-11-17 17:43:08 +0100

    config connection to atlas mongodb

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+require('dotenv').config(); // env variables
 
-const url = 'mongodb://localhost:27017/TodoApp'
+const uri = process.env.ATLAS_URI || 'mongodb://localhost:27017/TodoAPI-DB'
 mongoose.Promise = global.Promise;
 
-mongoose.connect(url).catch(err => console.log(err));
+mongoose.connect(uri,{ useNewUrlParser: true,useUnifiedTopology:true }).catch(err => console.log(err));
 
 module.exports = {mongoose};
