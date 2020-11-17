f0b9c65b13c8d7fbf96cb6c2a3eb124d24a8dbda
melanas <anas.melk96@gmail.com>
2020-11-17 17:56:19 +0100

    fixed require mongoDb to mongodb error

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const {mongoose} = require('./db/mongoose');
-const {ObjectID} = require('mongoDb');
+const {ObjectID} = require('mongodb');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
 
