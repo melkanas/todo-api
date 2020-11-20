28bdac021bb1356901df44c2aa9d6b86624f290d
melanas <anas.melk96@gmail.com>
2020-11-20 19:38:16 +0100

    added patch route

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -50,6 +50,23 @@ app.get('/todos/:id',(req,res)=>{
         .catch(err => res.status(404).send({error:'internal error'}));
 })
 
+app.patch('/todos/:id',(req,res)=>{
+    let {id} = req.params;
+    let body = _.pick(req.body,['text','completed']);
+    if(!ObjectID.isValid(id))
+        return res.status(404).send();
+    if(_.isBoolean(body.completed) && body.completed)
+        body.completedAt = new Date().getTime();
+    else{
+        body.completed = false;
+        body.completedAt = null;
+    }
+    Todo.findByIdAndUpdate(id,{$set:body},{$new:true}).then((todo)=>{
+        if(!todo)
+           return res.status(404).send()
+        res.send({todo})
+    }).catch(e=> res.status(404).send());
+})
 app.listen(port,()=> console.log('app listening on port: ',port));
 
 module.exports = {app};
