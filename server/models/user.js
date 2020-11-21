8f398a7738e372e857f5cba67af078574c4609ae
melanas <anas.melk96@gmail.com>
2020-11-21 19:27:26 +0100

    added get/users/me and auth middleware

--- server/models/user.js
+++ server/models/user.js
@@ -35,7 +35,7 @@ UserSchema.methods.toJSON = function(){
     let userObj = user.toObject();
     return _.pick(userObj,['_id','email']);
 }
-
+//instance methods
 UserSchema.methods.generateAuthToken = function(){
     let user = this;
     let access = 'auth';
@@ -45,7 +45,21 @@ UserSchema.methods.generateAuthToken = function(){
         return token
     });
 }
-
+//model methods(statics)
+UserSchema.statics.findByToken = function(token){
+    let User = this;
+    let decoded;
+    try{
+        decoded = jwt.verify(token,'secretsalt');
+    }catch(err){
+        return Promise.reject();
+    }
+    return User.findOne({
+        '_id':decoded._id,
+        'tokens.token':token,
+        'tokens.access':'auth'
+    });
+}
 let User = mongoose.model('User',UserSchema);
 
 module.exports = {User};
--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const {mongoose} = require('./db/mongoose');
 const {ObjectID} = require('mongodb');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
+const {authenticate} = require('./middleware/authenticate');
 
 let port = process.env.PORT;
 
@@ -85,6 +86,12 @@ app.post('/users',(req,res)=>{
         })
         .catch(err => res.status(400).send(err));
 });
+
+
+
+app.get('/users/me',authenticate,(req,res)=>{
+    res.send(req.user);
+})
 
 app.listen(port,()=> console.log('app listening on port: ',port));
 
