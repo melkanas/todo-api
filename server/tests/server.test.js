347474ed1b0871ff23a0e9b5eb9e74b9041d5f56
melanas <anas.melk96@gmail.com>
2020-11-29 23:36:12 +0100

    added test for Delete/users/me/token route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -220,4 +220,21 @@ describe('Post users/login',()=>{
         });
 
     });
+});
+describe('DELETE users/me/token',()=>{
+    it('should remove auth token on logout',(done)=>{
+        request(app)
+        .delete('/users/me/token')
+        .set('x-auth',users[0].tokens[0].token)
+        .expect(200)
+        .expect(res => expect(res.body).toContain({message:'logged out successfully'}))
+        .end((err,res)=>{
+            if(err)
+                return done(err);
+            User.findById(users[0]._id.toHexString()).then(doc =>{
+                expect(doc.tokens.length).toBe(0);
+                done();
+            }).catch(err=>done(err));
+        });
+    });
 })
